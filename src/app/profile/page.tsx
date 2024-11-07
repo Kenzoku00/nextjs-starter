5effdce5d9f7dbe6218828376bedb1c11cccb6c8
Kenzoku00 <155788641+Kenzoku00@users.noreply.github.com>
2024-11-07 07:49:48 +0700

    iyh

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,12 @@
-import { Avatar, Button, Flex, Heading, Icon, Tag, Text, Arrow } from '@/once-ui/components';
-import { baseURL, renderContent } from '@/app/resources';
+import { Avatar, Button, Flex, Heading, Icon, Tag, Text, Arrow, SmartImage } from '@/once-ui/components';
+import { baseURL } from '@/app/resources';
 import TableOfContents from '@/components/about/TableOfContents';
 import styles from '@/components/about/about.module.scss';
-import Link from 'next/link';  // Import Link for navigation
+import Link from 'next/link';
 
 export async function generateMetadata() {
-    const title = "Profile"; // Ganti dengan judul yang sesuai
-    const description = "This is the profile page."; // Ganti dengan deskripsi yang sesuai
+    const title = "Profile";
+    const description = "This is the profile page.";
     const ogImage = `https://${baseURL}/og?title=${encodeURIComponent(title)}`;
 
     return {
@@ -17,12 +17,7 @@ export async function generateMetadata() {
             description,
             type: 'website',
             url: `https://${baseURL}/about`,
-            images: [
-                {
-                    url: ogImage,
-                    alt: title,
-                },
-            ],
+            images: [{ url: ogImage, alt: title }],
         },
         twitter: {
             card: 'summary_large_image',
@@ -35,10 +30,10 @@ export async function generateMetadata() {
 
 export default function About() {
     const person = {
-        name: "John Doe",
-        role: "Developer",
-        location: "City",
-        languages: ["English"],
+        name: "Ilyasa Daffa Saskara",
+        role: "Game Developer",
+        location: "Jawa Timur, Indonesia",
+        languages: ["English", "Indonesia"],
         avatar: "/images/p3.png"
     };
 
@@ -46,30 +41,73 @@ export default function About() {
         intro: {
             title: "Introduction",
             display: true,
-            description: "This is the introduction."
+            description: "Saya pelajar yang sedang belajar dalam dunia pengembangan game. Saya selalu berusaha untuk belajar hal baru dan mengasah keterampilan saya. Saya juga ikut berkolaborasi dengan rekan - rekan dalam proyek untuk mengasah keterampilan dan membantu satu sama lain dalam suatu proyek."
         },
         work: {
-            title: "Work Experience",
+            title: "Game Project",
             display: true,
-            experiences: [
-                { company: "Company A", role: "Role A" },
-                { company: "Company B", role: "Role B" }
-            ]
-        },
-        studies: {
-            title: "Education",
-            display: true,
-            institutions: [
-                { name: "Institution A" },
-                { name: "Institution B" }
-            ]
-        },
-        technical: {
-            title: "Technical Skills",
-            display: true,
-            skills: [
-                { title: "Skill A", description: "Description A" },
-                { title: "Skill B", description: "Description B" }
+            projects: [
+                {
+                    title: "Guess Strange Animal",
+                    date: "Jan 2024 - Jan 2024",
+                    role: "Game Design",
+                    description: [
+                        "Game yang dibuat untuk event Global Game Jam Malang 2024.",
+                        "Game ini dibuat dalam waktu 3 hari, saya bertugas untuk mencari gambar hewan aneh dan merancang hint untuk game ini.",
+                        "Merupakan game teka teki bergambar, yang dimana nantinya pemain akan menebak berbagai gambar hewan aneh dengan hint yang tersedia."
+                    ],
+                    images: ["/images/GSA/GSA 6.png"]
+                },
+                {
+                    title: "A Way Up",
+                    date: "Jan 2024 - Feb 2024",
+                    role: "Indie Developer",
+                    description: [
+                        "Sebuah game petualangan 3D yang terinspirasi dari game Only Up.",
+                        "Pemain akan menjelajahi dunia mimpi yang di dalamnya terdapat berbagai rintangan untuk mencapai titik teratas.",
+                    ],
+                    images: ["/images/AWU/AWU 1.png", "/images/AWU/AWU 2.png"]
+                },
+                {
+                    title: "Lunc Zombies",
+                    date: "Mar 2024 - May 2024",
+                    role: "Programmer",
+                    description: [
+                        "Sebuah game petualangan platformer untuk windows.",
+                        "Pemain mempunyai misi untuk membunuh semua musuh dan mengalahkan boss di setiap level.",
+                    ],
+                    images: ["/images/LZ/LZ 1.png", "/images/LZ/LZ 2.png"]
+                },
+                {
+                    title: "Bouncy Ball",
+                    date: "Jun 2024 - Agu 2024",
+                    role: "Programmer",
+                    description: [
+                        "Sebuah game puzzle 3D untuk platform mobile.",
+                        "Mengharuskan pemain untuk melewati level dengan menyelesaikan puzzle.",
+                    ],
+                    images: ["/images/BB/BB 1.png", "/images/BB/BB 2.png"]
+                },
+                {
+                    title: "Chess Empire",
+                    date: "Agu 2024 - Sep 2024",
+                    role: "Designer",
+                    description: [
+                        "Game yang terinspirasi dari game Shotgun King yang kami buat untuk platform mobile.",
+                        "Memiliki lore yang dalam dan menarik, mengharuskan player untuk meningkatkan senjata dan perlengkapan untuk memudahkan pemain dalam menyelesaikan setiap level.",
+                    ],
+                    images: ["/images/CE/CE 3.png", "/images/CE/CE 4.png"]
+                },
+                {
+                    title: "Lost Case",
+                    date: "Oct 2024 - Sekarang",
+                    role: "Programmer dan Designer",
+                    description: [
+                        "Game berbasis story, puzzle, dan horror yang mengharuskan pemain berperan sebagai seorang detektif untuk memecahkan suatu kasus.",
+                        "Pemain diharuskan mencari clue untuk menamatkan game ini seiring dengan berjalannya cerita.",
+                    ],
+                    images: ["/images/LS/LS 1.png", "/images/LS/LS 2.png"]
+                }
             ]
         },
         tableOfContent: {
@@ -78,39 +116,15 @@ export default function About() {
         }
     };
 
-    const structure = [
-        {
-            title: about.intro.title,
-            display: about.intro.display,
-            items: []
-        },
-        {
-            title: about.work.title,
-            display: about.work.display,
-            items: about.work.experiences.map(experience => experience.company)
-        },
-        {
-            title: about.studies.title,
-            display: about.studies.display,
-            items: about.studies.institutions.map(institution => institution.name)
-        },
-        {
-            title: about.technical.title,
-            display: about.technical.display,
-            items: about.technical.skills.map(skill => skill.title)
-        },
-    ];
-
     return (
         <Flex fillWidth maxWidth="m" direction="column" style={{ position: 'relative' }}>
-            {/* Tombol Back To Home dengan posisi fixed */}
             <Link href="/">
                 <Flex
                     style={{
                         position: 'fixed',
-                        top: '3rem', // Jarak dari atas
-                        left: '3rem', // Jarak dari kiri
-                        zIndex: 1000, // Pastikan tombol berada di atas konten lain
+                        top: '3rem',
+                        left: '3rem',
+                        zIndex: 1000,
                     }}
                     marginBottom="m">
                     <Button
@@ -124,21 +138,25 @@ export default function About() {
                     </Button>
                 </Flex>
             </Link>
-            
+
             {about.tableOfContent.display && (
                 <Flex
                     style={{ position: 'fixed', left: '0', top: '50%', transform: 'translateY(-50%)', marginRight: '64px' }}
-                    paddingLeft="24" // Tambahkan padding kiri di sini
+                    paddingLeft="24"
                     gap="32"
                     direction="column" hide="s">
-                    <TableOfContents structure={structure} about={about} />
+                    <TableOfContents structure={[ 
+                        { title: about.intro.title, display: about.intro.display, items: [] },
+                        { title: about.work.title, display: about.work.display, items: about.work.projects.map(project => project.title) }
+                    ]} 
+                    about={about} />
                 </Flex>
             )}
 
             <Flex fillWidth mobileDirection="column" justifyContent="center" className={styles.flexContainer}>
                 <Flex
                     minWidth="160" paddingX="l" paddingBottom="xl" gap="m"
-                    flex={3} direction="column" alignItems="center" marginTop="128"> {/* Mengganti "4xl" dengan 4 */}
+                    flex={3} direction="column" alignItems="center" marginTop="128">
                     <Avatar src={person.avatar} size="xl" />
                     <Flex gap="8" alignItems="center">
                         <Icon onBackground="accent-weak" name="globe" />
@@ -154,16 +172,17 @@ export default function About() {
                         </Flex>
                     )}
                 </Flex>
-                <Flex className={`${styles.blockAlign} ${styles.top}`} fillWidth flex={9} maxWidth={40} direction="column"> {/* Menggabungkan className di sini */}
+
+                <Flex className={`${styles.blockAlign} ${styles.top}`} fillWidth flex={9} maxWidth={40} direction="column">
                     <Heading className={styles.textAlign} variant="display-strong-xl">
                         {person.name}
                     </Heading>
-                    <Text className={styles.textAlign} variant="display-default-xs" onBackground="neutral-weak">
+                    <Text className={styles.textAlign} variant="display-default-xs" onBackground="neutral-weak" marginTop='s'>
                         {person.role}
                     </Text>
-                    
+
                     {about.intro.display && (
-                        <Flex direction="column" textVariant="body-default-l" fillWidth gap="m" marginBottom="xl">
+                        <Flex direction="column" textVariant="body-default-l" fillWidth gap="m" marginBottom="xl" marginTop='s'>
                             {about.intro.description}
                         </Flex>
                     )}
@@ -173,48 +192,50 @@ export default function About() {
                             <Heading as="h2" id={about.work.title} variant="display-strong-s" marginBottom="m">
                                 {about.work.title}
                             </Heading>
-                            {about.work.experiences.map((experience, index) => (
-                                <Flex key={index} fillWidth direction="column">
-                                    <Text variant="heading-strong-l">
-                                        {experience.company}
-                                    </Text>
-                                    <Text variant="body-default-s" onBackground="brand-weak" marginBottom="m">
-                                        {experience.role}
-                                    </Text>
-                                </Flex>
-                            ))}
-                        </Flex>
-                    )}
-
-                    {about.studies.display && (
-                        <Flex direction="column" fillWidth gap="l" marginBottom="40">
-                            <Heading as="h2" id={about.studies.title} variant="display-strong-s" marginBottom="m">
-                                {about.studies.title}
-                            </Heading>
-                            {about.studies.institutions.map((institution, index) => (
-                                <Flex key={index} fillWidth direction="column">
-                                    <Text variant="heading-strong-l">
-                                        {institution.name}
-                                    </Text>
-                                </Flex>
-                            ))}
-                        </Flex>
-                    )}
-
-                    {about.technical.display && (
-                        <Flex direction="column" fillWidth gap="l" marginBottom="40">
-                            <Heading as="h2" id={about.technical.title} variant="display-strong-s" marginBottom="m">
-                                {about.technical.title}
-                            </Heading>
-                            {about.technical.skills.map((skill, index) => (
-                                <Flex key={index} fillWidth direction="column">
-                                    <Text variant="heading-strong-l">
-                                        {skill.title}
-                                    </Text>
-                                    {skill.description && (
-                                        <Text variant="body-default-s" onBackground="brand-weak" marginBottom="m">
-                                            {skill.description}
+                            {about.work.projects.map((project, index) => (
+                                <Flex key={index} fillWidth direction="column" marginBottom="32">
+                                    <Flex justifyContent="space-between" alignItems="center">
+                                        <Text variant="heading-strong-xl">
+                                            {project.title}
                                         </Text>
+                                        <Text variant="body-default-s" style={{ color: 'gray' }}>
+                                            {project.date}
+                                        </Text>
+                                    </Flex>
+                                    <Text variant="body-default-s" style={{ color: 'blue', fontSize: '1em' }} marginBottom="s">
+                                        {project.role}
+                                    </Text>
+                                    <Flex direction="column" gap="m" marginBottom="m">
+                                        {project.description.map((point, idx) => (
+                                            <Text key={idx} variant="body-default-m" style={{ fontSize: '1em' }}>
+                                                <span style={{ color: 'blue', marginRight: '8px' }}>•</span>
+                                                {point}
+                                            </Text>
+                                        ))}
+                                    </Flex>
+                                    {/* Render Gambar */}
+                                    {project.images && project.images.length > 0 && (
+                                        <Flex fillWidth paddingTop="m" paddingLeft="40" wrap>
+                                            {project.images.map((image, imgIdx) => (
+                                                <Flex
+                                                    key={imgIdx}
+                                                    border="neutral-medium"
+                                                    borderStyle="solid-1"
+                                                    radius="m"
+                                                    maxWidth={1920}
+                                                    style={{ position: 'relative', width: '254px', height: '142px', marginRight: '16px' }}
+                                                    className={styles.imagetop}
+                                                >
+                                                    <SmartImage
+                                                        enlarge
+                                                        radius="m"
+                                                        alt={`${project.title} image ${imgIdx + 1}`}
+                                                        src={image}
+                                                        sizes="(max-width: 1920px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                                    />
+                                                </Flex>
+                                            ))}
+                                        </Flex>
                                     )}
                                 </Flex>
                             ))}
